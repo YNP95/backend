d439dafc02d28eee6bef93154358716c374687c0
HyeonJun Yoon <hjyoun0731@gmail.com>
2023-12-07 00:29:54 +0900

    update db open

--- api/mysql.go
+++ api/mysql.go
@@ -24,7 +24,7 @@ type Users struct {
 func NewTable() error {
 	db := NewDb()
 	defer db.Close()
-	_, err := db.Exec("CREATE TABLE IF NOT EXISTS USERS (USER_ID int NOT NULL, PW binary NOT NULL, NAME varchar(45) NOT NULL, EMAIL varchar(100) NOT NULL, TEL varchar(45) DEFAULT NULL, LAST_ACCESS_DT datetime DEFAULT NULL, UPDATE_DT datetime DEFAULT NULL, CREATE_DT datetime DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (USER_ID) ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;")
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS USERS (USER_ID int NOT NULL, PW binary(100) NOT NULL, NAME varchar(45) NOT NULL, EMAIL varchar(100) NOT NULL, TEL varchar(45) DEFAULT NULL, LAST_ACCESS_DT datetime DEFAULT NULL, UPDATE_DT datetime DEFAULT NULL, CREATE_DT datetime DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (USER_ID) ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;")
 	if err != nil {
 		log.Println("create table fail.")
 		return errors.New("create table fail")
@@ -33,7 +33,7 @@ func NewTable() error {
 }
 
 func NewDb() *sql.DB {
-	db, err := sql.Open("mysql", "root:ROOTroot12#$@tcp(mysql)/ghldnjs?parseTime=true")
+	db, err := sql.Open("mysql", "root:ROOTroot12#$@tcp(127.0.0.1:3306)/ghldnjs?parseTime=true")
 	if err != nil {
 		log.Println(err)
 	}
