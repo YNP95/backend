36937f55487ccdee80daf5450a949ffadf996d1d
HyeonJun Yoon <hjyoun0731@gmail.com>
2023-12-20 00:01:45 +0900

    jwt test hold

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"errors"
 	"log"
-	"net/http"
 	"time"
 
 	"ynp/api"
 	"ynp/env"
 
-	"github.com/golang-jwt/jwt"
-	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	echoSwagger "github.com/swaggo/echo-swagger"
@@ -56,19 +52,20 @@ func main() {
 		Format: "method=${method}, uri=${uri}, status=${status}\n",
 	}))
 
-	e.Use(echojwt.JWT([]byte("secret")))
+	// e.Use(echojwt.JWT([]byte("secret")))
 
-	e.GET("/v1/jwttest", func(c echo.Context) error {
-		token, ok := c.Get("user").(*jwt.Token) // by default token is stored under `user` key
-		if !ok {
-			return errors.New("JWT token missing or invalid")
-		}
-		claims, ok := token.Claims.(jwt.MapClaims) // by default claims is of type `jwt.MapClaims`
-		if !ok {
-			return errors.New("failed to cast claims as jwt.MapClaims")
-		}
-		return c.JSON(http.StatusOK, claims)
-	})
+	// e.GET("/", func(c echo.Context) error {
+	// 	token, ok := c.Get("user").(*jwt.Token) // by default token is stored under `user` key
+	// 	if !ok {
+	// 		return errors.New("JWT token missing or invalid")
+	// 	}
+	// 	claims, ok := token.Claims.(jwt.MapClaims) // by default claims is of type `jwt.MapClaims`
+	// 	if !ok {
+	// 		return errors.New("failed to cast claims as jwt.MapClaims")
+	// 	}
+	// 	return c.JSON(http.StatusOK, claims)
+	// })
+
 	// e.Use(echojwt.WithConfig(echojwt.Config{
 	// 	// ...
 	// 	SigningKey:             []byte("secret"),
